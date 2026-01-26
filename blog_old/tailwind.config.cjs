4b18c605500c4aab024d83d8508bcf42b6a32a16
Ansh Tulsyan <atulsyan850@gmail.com>
2026-01-25 23:58:09 -0600

    Deploy site to GitHub Pages

--- blog_old/tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-const preset = require("../tailwind.preset.cjs");
-
-module.exports = {
-  presets: [preset],
-  content: ["./pages/**/*.{mdx,md}", "./src/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}"],
-};
--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-import preset from "./tailwind.preset.cjs";
-
-export default {
-  darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  prefix: "",
-  presets: [preset],
-} satisfies Config;
